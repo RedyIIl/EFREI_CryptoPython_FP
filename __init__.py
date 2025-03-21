9e33c95d770c690943b239f0c827f0163f25b1c2
Redy <110985447+RedyIIl@users.noreply.github.com>
2025-03-21 10:40:07 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
                                                                                                                                        
 @app.route('/')
 def hello_world():
-    return render_template('hello.html') #GITK
+    return render_template('hello.html') #TestComGITK
 
 key = Fernet.generate_key()
 f = Fernet(key)
